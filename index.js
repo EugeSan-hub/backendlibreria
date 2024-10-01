0b4df8c36ddd62b4b8d11106ee8f61d86eb5baf0
Eugenia Sansierra <eugesans@gmail.com>
2024-10-01 15:20:03 -0300

    agregamos un index.html como archivo estatico

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-import express from 'express'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-console.log('hola mundo')
-// 1- configurar un puerto 
+console.log("hola mundo");
+// 1- configurar un puerto
+const app = express();
 
-//2- configurar middlewares 
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.info("Estoy escuchando el puerto " + app.get("port"));
+});
 
-// 3- configurar las rutas 
+//2- configurar middlewares
+app.use(morgan("dev")); // nos da informacion extra en la terminar
+app.use(cors()); // permite conexiones remotas
+app.use(express.json()); //interpretar losd ato en formato json de la solicitud
+app.use(express.urlencoded({ extended: true }));
+
+// 3- configurar las rutas
+// http://localhost:4000/prueba
+app.get("/prueba", (req, res, next) => {
+  console.log("alguien hizo una solicitud get a la ruta de prueba");
+  res.send("hola mundok desde el backend");
+});
