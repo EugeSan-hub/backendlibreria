a4352f9ba3222b8c5f99a506e34c5f7e1afd5047
Eugenia Sansierra <eugesans@gmail.com>
2024-10-09 20:12:34 -0300

    quede a medias min 33 de clase de editar productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,38 +1,46 @@
 import Producto from "../database/model/productos.js";
 
-
-Producto
-export const funcionPrueba = (req, res)=>{
-    console.log('alguien hizo una solicitud get a la ruta de prueba')
-    res.send('Hola mundo desde el backend')
-}
-
-export const crearProducto = async (req, res) => {
-try {
-  //extraer el producto del body de la solicitud (request)
-      //validar los datos del body
-  //crear un objeto con el modelo producto Producto
-const productoNuevo = new Producto(req.body);
-  //guardar el objeto en la BD
-await productoNuevo.save()
-  //enviar la respuesta que pdimos crear el prodcuto
-res.status(201).json({mensaje:'El producto fue creado correctamente'})
-} catch (error) {
-  console.error(error)
-  res.status(500).json({mensaje:'ocurrio un error,no se pudo crear el producto'})
-}
+Producto;
+export const funcionPrueba = (req, res) => {
+  console.log("alguien hizo una solicitud get a la ruta de prueba");
+  res.send("Hola mundo desde el backend");
 };
 
-export const listarProductos = async(req, res)=>{
+export const crearProducto = async (req, res) => {
   try {
-   
-   //pedir a la BD la coleccion de productos
-   const productos = await Producto.find()
-   //enviar la respuesta que pudimos crear el producto
-   res.status(200).json(productos)
+    //extraer el producto del body de la solicitud (request)
+    //validar los datos del body
+    //crear un objeto con el modelo producto Producto
+    const productoNuevo = new Producto(req.body);
+    //guardar el objeto en la BD
+    await productoNuevo.save();
+    //enviar la respuesta que pdimos crear el prodcuto
+    res.status(201).json({ mensaje: "El producto fue creado correctamente" });
   } catch (error) {
-   console.error(error)
-   res.status(500).json({mensaje: 'Ocurrio un error, no se pudo crear el producto'})
+    console.error(error);
+    res
+      .status(500)
+      .json({ mensaje: "ocurrio un error,no se pudo crear el producto" });
   }
-  
-}
+};
+
+export const listarProductos = async (req, res) => {
+  try {
+    //pedir a la BD la coleccion de productos
+    const productos = await Producto.find();
+    //enviar la respuesta que pudimos crear el producto
+    res.status(200).json(productos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Ocurrio un error, no se pudo crear el producto" });
+  }
+};
+
+export const editarProducto = async (req, res) => {
+  try {
+    console.log(req.body)
+    console.log(req.params.id)
+  } catch (error) {
+    res.status(500).json({ mensaje: "Ocurrio un error, no se pudo editar el producto" });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { crearProducto, funcionPrueba, listarProductos } from "../controllers/productos.controllers.js";
+import { crearProducto, funcionPrueba, listarProductos, editarProducto } from "../controllers/productos.controllers.js";
 
 const router =Router();
 router.route('/prueba').get(funcionPrueba)
 router.route('/productos').post(crearProducto).get(listarProductos)
-
+router.route('/productos/:id').put(editarProducto)
 export default router
