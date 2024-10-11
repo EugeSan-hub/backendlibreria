fde20d3bbb08f02309d8af3ddb1353da0509d4cc
Eugenia Sansierra <eugesans@gmail.com>
2024-10-11 14:57:05 -0300

    verificamos si el producto existe y luego lo enviamos como respuesta

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
-import { crearProducto, funcionPrueba, listarProductos, editarProducto, borrarProducto } from "../controllers/productos.controllers.js";
+import {
+  crearProducto,
+  funcionPrueba,
+  listarProductos,
+  editarProducto,
+  borrarProducto,
+  obtenerProducto,
+} from "../controllers/productos.controllers.js";
 
-const router =Router();
-router.route('/prueba').get(funcionPrueba)
-router.route('/productos').post(crearProducto).get(listarProductos)
-router.route('/productos/:id').put(editarProducto).delete(borrarProducto)
+const router = Router();
+router.route("/prueba").get(funcionPrueba);
+router.route("/productos").post(crearProducto).get(listarProductos);
+router
+  .route("/productos/:id")
+  .put(editarProducto)
+  .delete(borrarProducto)
+  .get(obtenerProducto);
 
-export default router
+export default router;
