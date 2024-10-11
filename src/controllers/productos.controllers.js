c3fb89d61c0651b65228bf08d63db5a8bb06eae9
Eugenia Sansierra <eugesans@gmail.com>
2024-10-11 14:35:14 -0300

    agregamos la logica par averficar si el producto existe y borrar

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -50,10 +50,25 @@ if (!productoBuscado) {
 } 
     // si lo encontre entonces lo edito
     await Producto.findByIdAndUpdate(req.params.id,req.body)
-
     //envio respuesta al frontend
     res.status(200).json({mensaje:'El producto fue editado correctamente'})
   } catch (error) {
     res.status(500).json({ mensaje: "Ocurrio un error, no se pudo editar el producto" });
   }
 };
+
+export const borrarProducto = async (req,res) =>  {
+  try {
+    //verficar si el ID es valido
+    const productoBuscado= await Producto.findById(req.params.id)
+    // si el id no existe enviar un mensaje de error
+    if(!productoBuscado){
+      return res.status(404).json({mensaje:'el producto no fue encontrado'})
+    }
+    //siexiste el producto con el id lo borro y respondo al frontend
+    await Producto.findByIdAndDelete(req.params.id);
+    res.status(200).json({mensaje:'el producto fue eliminado correctamente'})
+  } catch (error) {
+    res.status(500).json({ mensaje: "Ocurrio un error, no se pudo borrar el producto" })
+  }
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearProducto, funcionPrueba, listarProductos, editarProducto } from "../controllers/productos.controllers.js";
+import { crearProducto, funcionPrueba, listarProductos, editarProducto, borrarProducto } from "../controllers/productos.controllers.js";
 
 const router =Router();
 router.route('/prueba').get(funcionPrueba)
 router.route('/productos').post(crearProducto).get(listarProductos)
-router.route('/productos/:id').put(editarProducto)
+router.route('/productos/:id').put(editarProducto).delete(borrarProducto)
 
 export default router
